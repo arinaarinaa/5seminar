b9d4ab198e0975c9e2a393cfb7c3a8b8e145efd4
arinach <arina.tchukurova@yandex.ru>
2022-09-04 02:10:35 +0300

    add third

---
+++ third/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между 
+//максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+int [] array = {1,4,6,8,9};
+int maxx = array[0];
+int minn = array[0];
+for(int i=1; i<array.Length;i++)
+{
+    if(maxx<array[i])
+    {
+        maxx = array[i];
+        
+    }
+    if(minn>array[i])
+    {
+         minn = array[i];
+        
+
+    }
+    
+}
+Console.WriteLine(minn);
+Console.WriteLine(maxx);
+int result = maxx - minn;
+Console.WriteLine(result);
