d3ba094f6f37c2080105d1c8722e651b56e0233a
arinach <arina.tchukurova@yandex.ru>
2022-09-04 01:22:05 +0300

    add first

---
+++ first/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[size];
+Random rand = new Random();
+for(int i = 0; i<array.Length;i++)
+{
+    array[i] = rand.Next(100,1000);
+}
+Console.WriteLine(string.Join(",", array));
+int count = 0;
+foreach(int a in array)
+{
+    if(a%2==0)
+    {
+        count++;
+
+    }
+
+}
+Console.WriteLine(count);
