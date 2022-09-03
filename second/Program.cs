f367e38eda83afe747301bbd12681f37a2858d7f
arinach <arina.tchukurova@yandex.ru>
2022-09-04 01:43:09 +0300

    add second

---
+++ second/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+int [] array = new int[6];
+Random rand = new Random();
+for(int i = 0; i<array.Length; i++)
+{
+    array[i] = rand.Next(-15, 15);
+}
+Console.WriteLine(string.Join(",", array));
+
+
+int result =0;
+for(int i=0; i<array.Length; i++)
+{
+    if(i%2!=0)
+    {
+        result +=array[i];
+        i++;
+    }
+}
+Console.WriteLine(result);
